655ec2a20d0374bad98e9e5c45b8919773e1c6b6
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-08-11 19:56:08 -0500

    Add files via upload

---
+++ src/main/java/com/codingdojo/date/controllers/DateController.java
@@ -0,0 +1,27 @@
+package com.codingdojo.date.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Controller
+
+public class DateController {
+    @RequestMapping("/date")
+
+    public String date(Model model){
+        String dateString = new SimpleDateFormat("E, dd, MMMM, yyyy").format(new Date());
+        model.addAttribute("date", dateString);
+        return "date.jsp";
+    }
+    @RequestMapping("/time")
+    public String time(Model model){
+        String timeString = new SimpleDateFormat("hh:mm:ss a").format(new Date());
+        model.addAttribute("time", timeString);
+        return "time.jsp";
+    }
+}
